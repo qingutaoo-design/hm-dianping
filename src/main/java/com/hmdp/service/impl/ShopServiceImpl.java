45a6958ee56dc3ff3c96bc68c1d8480f8166bf25
QinGutaoo <qingutaoo@gmail.com>
2026-04-10 01:19:51 +0800

    商户信息缓存业务代码开发

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,17 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -16,5 +23,35 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
+
+    @Autowired
+    private ShopMapper shopMapper;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result getById(Long id) {
+
+        String cacheKey = RedisConstants.CACHE_SHOP_KEY + id;
+
+        //先根据id查询缓存
+        String shopInfo = stringRedisTemplate.opsForValue().get(cacheKey);
+
+        if(!shopInfo.isEmpty()) {
+            //如果缓存中有数据，直接返回
+            Shop shop = JSONUtil.toBean(shopInfo, Shop.class);
+            return Result.ok(shop);
+        }
+        //如果缓存中没有数据，根据id查询数据库
+        Shop shop = shopMapper.selectById(id);
+        if(shop == null) {
+            //如果数据库中没有数据，返回错误信息
+            return Result.fail("商铺不存在");
+        }
+        //如果数据库中有数据，将数据写入缓存，并设置过期时间
+        stringRedisTemplate.opsForValue().set(cacheKey, JSONUtil.toJsonStr(shop));
+        return Result.ok(shop);
+    }
+
 
 }
