6324e399e1fee859947915ae3fb67f88e1536dc3
QinGutaoo <qingutaoo@gmail.com>
2026-04-20 22:19:12 +0800

    展示单个博客具体内容业务代码开发

--- src/main/java/com/hmdp/service/IBlogService.java
+++ src/main/java/com/hmdp/service/IBlogService.java
@@ -1,7 +1,10 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +16,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IBlogService extends IService<Blog> {
 
+    List<Blog> queryHotBlog(Integer current);
+
+    Result queryBlogById(Long id);
 }
